7a3f5446b4c84c422772a5156d8e9dd71edf6aaf
Deepin07 <140501766+Deepin07@users.noreply.github.com>
2024-09-19 15:12:52 +0530

    Add files via upload

---
+++ CodingPractice/src/BasicCoding/BasicForLoop.java
@@ -0,0 +1,13 @@
+package BasicCoding;
+
+public class BasicForLoop {
+
+    public static void main(String[] args) {
+        
+        for(int i = 0; i <= 10; i++){
+            System.out.println(i);
+        }
+
+    }
+
+}
